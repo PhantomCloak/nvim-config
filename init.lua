a3528bf77e22f6f0445dc2ff7b31e9c3539aa763
Phantom <ph4nt0m@Phantoms-Air.lan>
2023-07-13 12:50:31 +0300

    simplify config

--- init.lua
+++ init.lua
@@ -19,6 +19,7 @@ require 'diagnostics'
 require 'prompt'
 require 'git'
 
+-- VISUAL
 bqf = require("bqf")
 qf = require("qf")
 dressing = require("dressing")
@@ -27,6 +28,52 @@ lspfuzzy = require('lspfuzzy')
 bqf.setup()
 qf.setup()
 dressing.setup()
+
+-- OPTIONS
+vim.opt.termguicolors = true
+
+vim.api.nvim_set_hl(0, "NavicText",{default = true, bg = "#1f2430", fg = "#73d0ff"})
+vim.api.nvim_set_hl(0, "NavicIconsMethod",{default = true, bg = "#1f2430", fg = "#c078b8"})
+vim.api.nvim_set_hl(0, "NavicIconsClass",{default = true, bg = "#1f2430", fg = "#c078b8"})
+vim.api.nvim_set_hl(0, "NavicIconsNamespace",{default = true, bg = "#1f2430", fg = "#c078b8"}) 
+
+vim.o.signcolumn = 'yes:1'
+vim.wo.number = true
+vim.o.cmdheight = 0
+vim.o.pumheight = 15
+vim.o.cinkeys = '0{,0},0),0#,!^F,o,O,e.'
+vim.opt.termguicolors = true
+vim.g.loaded = 1
+vim.g.loaded_netrwPlugin = 1
+
+-- FORMATTING OPTIONS
+
+vim.o.expandtab = true
+vim.o.smartindent = true
+vim.opt.lazyredraw = true
+vim.o.backspace = 'indent,eol,start'
+
+-- SEARCH OPTIONS
+vim.o.ignorecase = true
+vim.o.smartcase = true
+vim.o.wrapscan = true
+
+-- Enable undo features, even after closing vim
+vim.o.undofile = true
+vim.o.undodir = os.getenv('HOME') .. '/.cache/nvim'
+vim.o.undolevels = 10000
+
+-- Move swapfiles and backupfiles to ~/.cache
+vim.o.directory = os.getenv('HOME') .. '/.cache/nvim'
+vim.o.backup = true
+vim.o.backupdir = os.getenv('HOME') .. '/.cache/nvim'
+
+-- Line options
+vim.o.scrolloff = 12
+
+-- TREESITTER CONTEXt
+
+vim.api.nvim_set_hl(0, "TreesitterContext",{default = false, bg = "#363c4c"})
 
 -- Auto indention with insert mode
 vim.cmd [[
